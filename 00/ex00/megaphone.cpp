56fd30a0d6dc02403afed71232ae94845fedf40c
Paulic <61714149+PaulicStudios@users.noreply.github.com>
2024-05-13 00:06:54 +0200

    00 ex00

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char **args)
+{
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int ind_arg = 1; ind_arg < argc; ind_arg++)
+		{
+			for (int ind_str = 0; args[ind_arg][ind_str]; ind_str++)
+				std::cout << (char)toupper(args[ind_arg][ind_str]);
+			std::cout << (ind_arg == argc - 1 ? "" : " ");
+		}
+		std::cout << std::endl;
+	}
+}
