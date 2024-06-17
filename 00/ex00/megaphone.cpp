6e6a0ee72d8bacf1c93443925b74b4406fe3e67a
pgrossma <pgrossma@student.42heilbronn.de>
2024-06-17 19:03:12 +0200

    fix ex00 not printing characters

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -3,14 +3,12 @@
 int main(int argc, char **args)
 {
 	if (argc < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
 	{
-		for (int ind_arg = 1; ind_arg < argc; ind_arg++)
-		{
-			for (int ind_str = 0; args[ind_arg][ind_str]; ind_str++)
-				std::cout << toupper(args[ind_arg][ind_str]);
-		}
-		std::cout << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
 	}
+	for (int ind_arg = 1; ind_arg < argc; ind_arg++)
+		for (int ind_str = 0; args[ind_arg][ind_str]; ind_str++)
+			std::cout << (char) std::toupper(args[ind_arg][ind_str]);
+	std::cout << std::endl;
 }
