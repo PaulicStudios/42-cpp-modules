9a5d6ce8a52a4be793e6cd62a926037247bf59a1
pgrossma <pgrossma@student.42heilbronn.de>
2024-06-20 16:48:37 +0200

    added 42 headers

--- 01/ex03/src/HumanA.cpp
+++ 01/ex03/src/HumanA.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 16:48:16 by pgrossma          #+#    #+#             */
+/*   Updated: 2024/06/20 16:48:18 by pgrossma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "HumanA.hpp"
 
 HumanA::HumanA(std::string name, Weapon &weapon) : _name(name), _weapon(weapon) {}
