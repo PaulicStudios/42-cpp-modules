8af30d23be52a82053ff4202c0936e51baf87dec
pgrossma <pgrossma@student.42heilbronn.de>
2024-06-20 16:47:11 +0200

    ex03

---
+++ 01/ex03/src/HumanB.cpp
@@ -0,0 +1,15 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) : _name(name), _weapon(nullptr) {}
+
+HumanB::~HumanB() {}
+
+void	HumanB::setWeapon(Weapon &weapon)
+{
+	_weapon = &weapon;
+}
+
+void	HumanB::attack()
+{
+	std::cout << _name << " attacks with his " << _weapon->getType() << std::endl;
+}
