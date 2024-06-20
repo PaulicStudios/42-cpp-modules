14416c0d1ab0dacd52d54477a9bdc2eef82d4b80
pgrossma <pgrossma@student.42heilbronn.de>
2024-06-20 18:55:02 +0200

    changed nullptr to NULL

--- 01/ex03/src/HumanB.cpp
+++ 01/ex03/src/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 16:48:19 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/06/20 16:48:20 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/06/20 18:18:17 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : _name(name), _weapon(nullptr) {}
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {}
 
 HumanB::~HumanB() {}
 
