3a98ddd1d9b14cf381d27d84519217a0e36f1e74
pgrossma <pgrossma@student.42heilbronn.de>
2024-07-23 19:37:25 +0200

    started with cpp module 02 ex00

---
+++ 02/ex00/src/Fixed.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/23 19:24:57 by pgrossma          #+#    #+#             */
+/*   Updated: 2024/07/23 19:36:06 by pgrossma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	_nbr = 0;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	_nbr = fixed.getRawBits();
+}
+
+Fixed& Fixed::operator=(const Fixed &fixed)
+{
+	std::cout << "Copy assigment operator called" << std::endl;
+	_nbr = fixed.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_nbr);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	_nbr = raw;
+}
