49fd422797896ae2afe4a043a72d5b14f3984495
pgrossma <pgrossma@student.42heilbronn.de>
2024-07-24 14:11:54 +0200

    copied ex00 to ex01

--- 02/ex01/inc/Fixed.hpp
+++ 02/ex01/inc/Fixed.hpp
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/24 14:10:38 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/07/24 14:10:40 by pgrossma         ###   ########.fr       */
+/*   Created: 2024/06/20 16:47:26 by pgrossma          #+#    #+#             */
+/*   Updated: 2024/07/02 17:59:49 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <string>
 #include <iostream>
-#include "Fixed.hpp"
 
-int	main( void )
-{
-	Fixed a;
-	Fixed b(a);
-	Fixed c;
-
-	c = b;
-
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
-	return 0;
-}
+class Fixed {
+	private:
+		int					_nbr;
+		static const int	_fractional_bits = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed &fixed);
+		Fixed& operator=(const Fixed &fixed);
+		~Fixed();
+		int		getRawBits() const;
+		void	setRawBits(int const raw);
+};
