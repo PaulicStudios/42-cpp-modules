c7bbb2715dedb476795bf173db67c9f4b6e8cc6e
pgrossma <pgrossma@student.42heilbronn.de>
2024-07-25 19:26:12 +0200

    added tests to ex02

--- 03/ex02/src/main.cpp
+++ 03/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 14:10:38 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/07/25 19:23:20 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/07/25 19:24:53 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 int	main( void )
 {
+	FragTrap	fragTrap1("FT 1");
+	FragTrap	fragTrap2;
 
+	fragTrap2 = FragTrap("FT 2");
+
+	fragTrap1.attack("target");
+	fragTrap1.highFiveGuys();
 
 	return 0;
 }
