80e9dc4d57fa892788516eecc702bbe2099898bb
pgrossma <pgrossma@student.42heilbronn.de>
2024-11-11 13:38:08 +0100

    improved main of ex00 and finished ex00

--- 05/ex00/main.cpp
+++ 05/ex00/main.cpp
@@ -15,33 +15,44 @@
 
 int main()
 {
-	Bureaucrat	bureaucrat("bureaucrat", 150);
+	Bureaucrat	bureaucrat("bureaucrat!", 150);
+	std::cout << "Printing out << of bureaucrat" << std::endl;
 	std::cout << bureaucrat << std::endl;
+
+	std::cout << "Printing out the Name" << std::endl;
 	std::cout << bureaucrat.getName() << std::endl;
+	std::cout << "Printing out the grade" << std::endl;
 	std::cout << bureaucrat.getGrade() << std::endl;
 
+	std::cout << "Trying to create Bureaucrat with a grad of 151" << std::endl;
 	try {
 		Bureaucrat	bureaucrat151("bureaucrat", 151);
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
+
+	std::cout << "Trying to create Bureaucrat with a grad of 0" << std::endl;
 	try {
 		Bureaucrat	bureaucrat0("bureaucrat", 0);
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
 
+	std::cout << "Decrementing grade of first bureaucrat" << std::endl;
 	try {
 		bureaucrat.decrementGrade();
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
+	std::cout << "Incrementing grade of first bureaucrat" << std::endl;
 	try {
 		bureaucrat.incrementGrade();
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
 	std::cout << bureaucrat << std::endl;
+
+	std::cout << "Decrementing grade of first bureaucrat" << std::endl;
 	try {
 		bureaucrat.decrementGrade();
 	} catch (std::exception &e) {
@@ -49,6 +60,7 @@ int main()
 	}
 	std::cout << bureaucrat << std::endl;
 
+	std::cout << "Created new Bureaucrat2" << std::endl;
 	Bureaucrat	bureaucrat2("bureaucrat2", 2);
 	std::cout << bureaucrat2 << std::endl;
 	try {
@@ -56,6 +68,7 @@ int main()
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
+
 	std::cout << bureaucrat2 << std::endl;
 	return (0);
 }
