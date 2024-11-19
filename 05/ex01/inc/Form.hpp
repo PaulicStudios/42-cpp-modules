7087130d90af9ca88a87a7b9386d1b9039dcf120
pgrossma <pgrossma@student.42heilbronn.de>
2024-11-19 15:34:15 +0100

    addde base Form class with constructors and getters

--- 05/ex01/inc/Form.hpp
+++ 05/ex01/inc/Form.hpp
@@ -6,8 +6,43 @@
 /*   By: pgrossma <pgrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 15:26:08 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/11/19 15:26:25 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:34:02 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <string>
+#include <exception>
+#include <iostream>
+
+class Form {
+    private:
+        const std::string _name;
+        bool _signed;
+        const uint _gradeToSign;
+        const uint _gradeToExecute;
+
+        Form();
+    
+    public:
+        Form(std::string name, uint gradeToSign, uint gradeToExecute);
+        Form(Form const &Form);
+        Form &operator=(Form const &Form);
+        ~Form();
+
+    class GradeTooHighException final : public std::exception {
+        public:
+            [[nodiscard]] const char *what() const noexcept override {
+                return ("Grade too high");
+            }
+    }
+    class GradeTooLowException final : public std::exception {
+        public:
+            [[nodiscard]] const char *what() const noexcept override {
+                return ("Grade too low");
+            }
+    }
+}
+
+std::ostream &operator<<(std::ostream &out, const Form &form);
--- 05/ex01/src/Form.cpp
+++ 05/ex01/src/Form.cpp
@@ -6,9 +6,49 @@
 /*   By: pgrossma <pgrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 15:26:01 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/11/19 15:26:02 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:32:42 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#include "Form.h"
+#include "Form.hpp"
+
+Form::Form() : _name("default"), _signed(false), _gradeToSign(1), _gradeToExecute(1) {}
+
+Form::Form(std::string name, uint gradeToSign, uint gradeToExecute) : _name(name), _signed(false), _gradeToSign(gradeToSign), _gradeToExecute(gradeToExecute) {
+    if (gradeToSign < 1 || gradeToExecute < 1) {
+        throw GradeTooHighException();
+    } else if (gradeToSign > 150 || gradeToExecute > 150) {
+        throw GradeTooLowException();
+    }
+}
+
+Form::Form(Form const &Form) : _name(Form._name), _signed(Form._signed), _gradeToSign(Form._gradeToSign), _gradeToExecute(Form._gradeToExecute) {}
+
+Form &Form::operator=(Form const &Form) {
+    this->_signed = Form._signed;
+    return (*this);
+}
+
+Form::~Form() = default;
+
+std::string Form::getName() const {
+    return (this->_name);
+}
+
+bool Form::getSigned() const {
+    return (this->_signed);
+}
+
+uint Form::getGradeToSign() const {
+    return (this->_gradeToSign);
+}
+
+uint Form::getGradeToExecute() const {
+    return (this->_gradeToExecute);
+}
+
+std::ostream &operator<<(std::ostream &out, const Form &form) {
+    out << "Form " << form.getName() << " is " << (form.getSigned() ? "signed" : "not signed") << " and requires grade " << form.getGradeToSign() << " to sign and grade " << form.getGradeToExecute() << " to execute";
+    return (out);
+}
