cd7040ace432836f79ce98c837083bf6aa075733
pgrossma <pgrossma@student.42heilbronn.de>
2024-11-20 16:32:10 +0100

    restructured to first check for type

--- 06/ex00/src/ScalarConverter.cpp
+++ 06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: pgrossma <pgrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:27:27 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/11/20 16:16:25 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:30:59 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,53 @@ ScalarConverter::~ScalarConverter() = default;
 
 void ScalarConverter::convert(const std::string &literal)
 {
-    print_char(literal);
+    Type type = getType(literal);
+
+    switch (type)
+    {
+        case CHAR:
+            printFromChar(literal);
+            break;
+        case INT:
+            printFromInt(literal);
+            break;
+        case FLOAT:
+            printFromFloat(literal);
+            break;
+        case DOUBLE:
+            printFromDouble(literal);
+            break;
+    }
 }
 
-void ScalarConverter::print_char(const std::string &literal)
+ScalarConverter::Type ScalarConverter::getType(const std::string &literal)
 {
-    std::cout << "char: ";
-    try
-    {
-        char c = static_cast<char>(std::stoi(literal));
+    if (literal.length() == 1 && literal[0] >= 32 && literal[0] <= 126)
+        return (CHAR);
+    return (INT);
+}
 
-        if (c >= 32 && c <= 126)
-            std::cout << c;
-        else
-            std::cout << "Non displayable";
-    }
-    catch (std::exception &e)
-    {
-        std::cout << e.what();
-    }
-    std::cout << std::endl;
+void ScalarConverter::printFromChar(const std::string &literal)
+{
+    char c = literal[0];
+
+    std::cout << "char: " << c << std::endl;
+    std::cout << "int: " << static_cast<int>(c) << std::endl;
+    std::cout << "float: " << static_cast<float>(c) << std::endl;
+    std::cout << "double: " << static_cast<double>(c) << std::endl;
+}
+
+void ScalarConverter::printFromInt(const std::string &)
+{
+    
+}
+
+void ScalarConverter::printFromFloat(const std::string &)
+{
+
+}
+
+void ScalarConverter::printFromDouble(const std::string &)
+{
+
 }
--- 06/ex00/src/ScalarConverter.hpp
+++ 06/ex00/src/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: pgrossma <pgrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:26:23 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/11/20 16:06:58 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:27:13 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,20 @@
 #include <iostream>
 
 class ScalarConverter {
+    enum Type {
+        CHAR,
+        INT,
+        FLOAT,
+        DOUBLE
+    };
+
     private:
-        static void print_char(const std::string &literal);
+        static Type getType(const std::string &literal);
+        static void printFromChar(const std::string &literal);
+        static void printFromInt(const std::string &literal);
+        static void printFromFloat(const std::string &literal);
+        static void printFromDouble(const std::string &literal);
+
     public:
         ScalarConverter();
         ScalarConverter(ScalarConverter const &ScalarConverter);
