4657b298409731d1dd70e803aaf2a5658cceaecd
Paul Großmann <git@paulgrossmann.de>
2024-11-21 15:07:51 +0100

    moved function implementation into header file

--- 07/ex02/src/Array.hpp
+++ 07/ex02/src/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 14:40:28 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/11/21 14:45:07 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/11/21 14:59:39 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,27 @@ class Array {
         unsigned int _size;
 
     public:
-        Array();
-        Array(unsigned int n);
-        Array(const Array &other);
-        Array &operator=(const Array &other);
-        ~Array();
+        Array() : _array(nullptr), _size(0) {}
+
+        Array(unsigned int n) : _array(new T[n]), _size(n) {}
+
+        Array(const Array &other) : _array(new T[other._size]), _size(other._size) {
+            for (size_t i = 0; i < _size; i++) {
+                _array[i] = other._array[i];
+            }
+        }
+
+        Array &operator=(const Array &other) {
+            delete[] _array;
+            _array = new T[other._size];
+            _size = other._size;
+            for (size_t i = 0; i < _size; i++) {
+                _array[i] = other._array[i];
+            }
+            return *this;
+        }
+
+        ~Array() {
+            delete[] _array;
+        }
 };
