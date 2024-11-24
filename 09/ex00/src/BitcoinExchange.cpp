677523722aaefd683414ffa60340a804f990298a
pgrossma <pgrossma@student.42heilbronn.de>
2024-11-24 13:10:30 +0100

    added loaddata

--- 09/ex00/src/BitcoinExchange.cpp
+++ 09/ex00/src/BitcoinExchange.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
+/*   By: pgrossma <pgrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 12:48:28 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/11/24 12:50:01 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/11/24 13:09:43 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,36 @@
 
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::BitcoinExchange() : _fileName("") {
-	this->_exchangeRates = std::map<std::time_point, double>();
-}
+BitcoinExchange::BitcoinExchange() : _fileName(""), _exchangeRates(_loadExchangeRates()) {}
 
-BitcoinExchange::BitcoinExchange(std::string fileName) : _fileName(fileName) {
-	this->_exchangeRates = std::map<std::time_point, double>();
-}
+BitcoinExchange::BitcoinExchange(std::string fileName) : _fileName(fileName), _exchangeRates(_loadExchangeRates()) {}
 
 BitcoinExchange::~BitcoinExchange() {}
 
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &src) {
-	this->_exchangeRates = src._exchangeRates;
-}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &src) : _fileName(src._fileName), _exchangeRates(src._exchangeRates) {}
 
 BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &src) {
-	this->_exchangeRates = src._exchangeRates;
 	return (*this);
+}
+
+std::map<std::time_t, double> BitcoinExchange::_loadExchangeRates() {
+	std::ifstream dataFile("data.csv");
+	if (!dataFile.is_open()) {
+		throw std::runtime_error("Could not open data file");
+	}
+
+	std::string line;
+	while (std::getline(dataFile, line)) {
+		std::string date = line.substr(0, line.find(','));
+		if (date == "date")
+			continue;
+		std::string rate = line.substr(line.find(',') + 1);
+		std::time_t time = std::mktime(std::localtime(&date));
+
+		if (time == -1 || date.empty() || rate.empty()) {
+			std::cerr << "Error: Invalid date or rate on line: " << line << std::endl;
+			continue;
+		}
+		_exchangeRates.insert(std::make_pair(time, std::stod(rate)));
+	}
 }
--- 09/ex00/src/BitcoinExchange.hpp
+++ 09/ex00/src/BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
+/*   By: pgrossma <pgrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 12:20:41 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/11/24 12:50:31 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/11/24 13:10:07 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 #include <map>
 #include <string>
-#include <chrono>
+#include <ctime>
+#include <fstream>
 
 class BitcoinExchange {
 	private:
 		const std::string _fileName;
-		std::map<std::time_point, double> _exchangeRates;
+		const std::map<std::time_t, double> _exchangeRates;
 
 		BitcoinExchange();
 
-		void _loadExchangeRates();
+		std::map<std::time_t, double> _loadExchangeRates();
 	public:
 		BitcoinExchange(std::string fileName);
 		~BitcoinExchange();
