2f89dc900e9308153c58f464835c72ae105cb18a
pgrossma <pgrossma@student.42heilbronn.de>
2024-11-28 20:01:11 +0100

    added try catch to main

--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -6,21 +6,29 @@
 /*   By: pgrossma <pgrossma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 13:11:33 by pgrossma          #+#    #+#             */
-/*   Updated: 2024/11/27 17:35:18 by pgrossma         ###   ########.fr       */
+/*   Updated: 2024/11/28 19:26:21 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+#include <exception>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    if (argc != 2) {
+    if (argc != 2)
+    {
         std::cout << "Usage: ./btc <inputfile>" << std::endl;
         return (EXIT_FAILURE);
     }
 
-    BitcoinExchange btc;
-    btc.lookUpValuesFromFile(argv[1]);
+    try
+    {
+        BitcoinExchange btc;
+        btc.lookUpValuesFromFile(argv[1]);
+    }
+    catch (std::exception &e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
 
     return (EXIT_SUCCESS);
 }
