db827c0919cae67d04b89e18e3e574b1b440b56f
Paul Großmann <git@paulgrossmann.de>
2025-01-11 12:25:24 +0100

    started removing comparisons for deque

--- 09/ex02/src/PmergeMe.hpp
+++ 09/ex02/src/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 12:58:37 by pgrossma          #+#    #+#             */
-/*   Updated: 2025/01/11 00:44:39 by pgrossma         ###   ########.fr       */
+/*   Updated: 2025/01/11 12:23:19 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,7 +93,7 @@ void PmergeMe<Container>::_sortPairs(uint level) {
     }
 
     uint pairSize = level * 2;
-    for (typename Container::iterator it = _nbrs.begin(); it < std::prev(_nbrs.end(), pairSize - 1); std::advance(it, pairSize)) {
+    for (typename Container::iterator it = _nbrs.begin(); it != std::prev(_nbrs.end(), _nbrs.size() % pairSize); std::advance(it, pairSize)) {
         typename Container::iterator pair1 = std::next(it, level - 1);
         typename Container::iterator pair2 = std::next(it, pairSize - 1);
         if (*pair1 > *pair2) {
@@ -123,7 +123,7 @@ void PmergeMe<Container>::_sortPairs(uint level) {
 template <typename Container>
 Container PmergeMe<Container>::_fillPend(uint level, uint pairSize) {
     Container pend;
-    for (typename Container::iterator it = std::next(_nbrs.begin(), pairSize - 1); it < _nbrs.end(); std::advance(it, pairSize)) {
+    for (typename Container::iterator it = std::next(_nbrs.begin(), pairSize - 1); it != std::prev(_nbrs.end(), _nbrs.size() % pairSize); std::advance(it, pairSize)) {
         if (std::next(it, pairSize - 1) >= _nbrs.end()) {
             break;
         }
@@ -168,7 +168,7 @@ template <typename Container>
 void PmergeMe<Container>::_insertPendUsingInsertionOrder(Container &pend, Container &insertionOrder, uint level) {
     for (typename Container::iterator it = insertionOrder.begin(); it != insertionOrder.end(); std::advance(it, 1)) {
         typename Container::iterator pendInd = std::next(pend.begin(), (*it - 1) * level - 1);
-        for (typename Container::iterator j = std::next(_nbrs.begin(), level - 1); j < _nbrs.end(); std::advance(j, level)) {
+        for (typename Container::iterator j = std::next(_nbrs.begin(), level - 1); j != std::prev(_nbrs.end(), _nbrs.size() % level); std::advance(j, level)) {
             if (*pendInd < *j) {
                 _nbrs.insert(std::prev(j, level - 1), std::prev(pendInd, level - 1), std::next(pendInd, 1));
                 break;
@@ -188,7 +188,7 @@ template <typename Container>
 void PmergeMe<Container>::_insertRemainingPend(Container &pend, uint level) {
     for (typename Container::iterator it = std::next(pend.begin(), level - 1); it < pend.end(); std::advance(it, level)) {
         bool inserted = false;
-        for (typename Container::iterator j = std::next(_nbrs.begin(), level - 1); j < _nbrs.end(); std::advance(j, level)) {
+        for (typename Container::iterator j = std::next(_nbrs.begin(), level - 1); j != std::prev(_nbrs.end(), _nbrs.size() % level); std::advance(j, level)) {
             if (*it < *j) {
                 _nbrs.insert(std::prev(j, level - 1), std::prev(it, level - 1), std::next(it, 1));
                 inserted = true;
